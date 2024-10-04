374da072d6e55b33c223f02e8972ba5ca6edcc0f
ajaz203 <ajazahmad203@gmail.com>
2024-10-05 03:02:20 +0530

    updates

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isNavbarCollapsed = true; // To track if the navbar is collapsed
+  activeDropdown: string | null = null; // To track which dropdown is active
 
-}
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed; // Toggle navbar state
+  }
+
+  toggleDropdown(dropdown: string) {
+    this.activeDropdown = this.activeDropdown === dropdown ? null : dropdown; // Toggle dropdown state
+  }
+  }
+
