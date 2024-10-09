16a22aebdc8b0e7c2c43bca94c3c568bdc8a8d4b
ajaz203 <ajazahmad203@gmail.com>
2024-10-10 03:02:18 +0530

    updates

--- src/app/pages/dmac/dmac.component.ts
+++ src/app/pages/dmac/dmac.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-dmac',
   templateUrl: './dmac.component.html',
   styleUrl: './dmac.component.scss'
 })
-export class DmacComponent {
+export class DmacComponent implements OnInit{
+  ngOnInit(): void {}
 
+  oneAtATime = true;
+  isFirstOpen = true;
 }
