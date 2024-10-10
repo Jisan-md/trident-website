c2309b4e50c59954d81e7e6876f5d2caa1a5a4ee
ajaz203 <ajazahmad203@gmail.com>
2024-10-10 23:18:03 +0530

    updates

--- src/app/pages/dmac/dmac.component.ts
+++ src/app/pages/dmac/dmac.component.ts
@@ -8,6 +8,29 @@ import { OnInit } from '@angular/core';
 export class DmacComponent implements OnInit{
   ngOnInit(): void {}
 
+  isFirstOpen: any;
   oneAtATime = true;
-  isFirstOpen = true;
+    
+      sections: { [key in 'dataCreation' | 'dataUpdate' | 'massDataManagement' | 'workflowApproval' | 'aiMlValidation' | 'dmac' | 'User']: boolean } = {
+      dataCreation: false,
+      dataUpdate: false,
+      massDataManagement: false,
+      workflowApproval: false,
+      aiMlValidation: false,
+      dmac: false,
+      User: false
+    };
+    toggleSection(section: 'dataCreation' | 'dataUpdate' | 'massDataManagement' | 'workflowApproval' | 'aiMlValidation'|'dmac' | 'User') {
+      const isCurrentlyOpen = this.sections[section]; 
+      console.log('Clicked Section:', section, 'Currently Open:', isCurrentlyOpen);
+  
+      // Close all sections
+      Object.keys(this.sections).forEach(key => {
+          this.sections[key as keyof typeof this.sections] = false;
+      });
+  
+      // If the clicked section was not open, open it
+      if (!isCurrentlyOpen) {
+          this.sections[section] = true; // Open the clicked section
+      }}
 }
